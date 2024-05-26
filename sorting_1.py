074600d58dd6b4aef644f3f5d2979596181add6d
Yarchyk3 <159487862+Yarchyk3@users.noreply.github.com>
2024-05-26 18:12:19 +0300

    task 5

---
+++ sorting_1.py
@@ -0,0 +1,13 @@
+def sorting_algorithm_1(list_of_numbers):
+    new_list = []
+    while len(list_of_numbers) > 0:
+        for x in list_of_numbers:
+            if x == min(list_of_numbers):
+                list_of_numbers.remove(x)
+                new_list.append(x)
+    return new_list
+
+
+print(sorting_algorithm_1([3, 2, 9, 7, 1, 4]))
+
+
